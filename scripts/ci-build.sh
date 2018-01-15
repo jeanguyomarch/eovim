bfbf8483662aace67ef51e452eb95ceb4af52ef5
Jean Guyomarc'h <jean@guyomarch.bzh>
2018-01-15 08:50:29 +0100

    ci: restore osx build and stop building exactness

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -7,26 +7,27 @@ set -u
 set -x
 
 sh -x ./scripts/get-msgpack.sh
-sh -x ./scripts/get-test-data.sh
+#sh -x ./scripts/get-test-data.sh
 
 mkdir -p build/_install && cd build
 
-install_dir="$(pwd)/_install"
+#install_dir="$(pwd)/_install"
+#
+## Install exactness
+#git clone https://git.enlightenment.org/tools/exactness.git
+#cd exactness
+#git checkout 62298eb8ffd50f9b470dd69e97e7a718d31b9fa9
+#autoreconf -fi
+#./configure --prefix "$install_dir"
+#make install
+#cd ..
+#
+## Make sure exactness is available
+#export PATH="$PATH:$install_dir/bin"
+#export LD_LIBRARY_PATH="$install_dir/lib"
 
-# Install exactness
-git clone https://git.enlightenment.org/tools/exactness.git
-cd exactness
-git checkout 62298eb8ffd50f9b470dd69e97e7a718d31b9fa9
-autoreconf -fi
-./configure --prefix "$install_dir"
-make install
-cd ..
-
-# Make sure exactness is available
-export PATH="$PATH:$install_dir/bin"
-export LD_LIBRARY_PATH="$install_dir/lib"
-
-cmake -DWITH_TESTS=ON -DCMAKE_INSTALL_PREFIX=_install ..
+#cmake -DWITH_TESTS=ON -DCMAKE_INSTALL_PREFIX=_install ..
+cmake -DWITH_TESTS=OFF -DCMAKE_INSTALL_PREFIX=_install ..
 cmake --build .
 cmake --build . --target install
 #ctest --output-on-failure # XXX For now
